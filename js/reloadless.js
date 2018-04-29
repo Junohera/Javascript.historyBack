1b81437d81d38c5ab2eded415f84cb110db3f3a3
junohera <wnsgh1110@naver.com>
2018-04-29 23:24:37 +0900

    주석 해제(back)

--- js/reloadless.js
+++ js/reloadless.js
@@ -1,10 +1,15 @@
 $(document).ready(function () {
+
   $(document).on('click', '.movePage a', function (event) {
+
     history.pushState(null, null, event.target.href);
+
     $('article').load(event.target.href+' article>.content');
+    
     event.preventDefault();
   })
-  // $(window).on('popstate', function(event){
-  //   $('article').load(location.href+' article>.content');
-  // })
+
+  $(window).on('popstate', function(event){
+    $('article').load(location.href+' article>.content');
+  })
 });
