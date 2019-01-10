ed579b08ee19a32cd423df961e5fe8b2f6af19f4
Juno <juno.address@gmail.com>
2019-01-10 21:00:10 +0900

    production 커밋

---
+++ build/precache-manifest.95831545f7412ec6baf351ab3578704b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d23c8e0a41d24dc5b7b",
+    "url": "/static/js/main.5d23c8e0.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "5d23c8e0a41d24dc5b7b",
+    "url": "/static/css/main.ca021cb9.chunk.css"
+  },
+  {
+    "revision": "8022c02a6a30412cb1dd752cc5ad4b88",
+    "url": "/index.html"
+  }
+];
